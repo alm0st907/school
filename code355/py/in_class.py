719f0cc6a9f78fdbba21e92ed02dfc15ea404774
dak0ta <garrett.rudisill@gmail.com>
2018-02-28 11:23:56 -0800

    string splitting

--- code355/py/in_class.py
+++ code355/py/in_class.py
@@ -1,8 +1,15 @@
 #python 3
 #garrett rudisill
+def other():
+    print("seeing if another function gets auto executed")
 def main():
     #the code goes here
     print("hello world")
+    string = input("Type a string here")
+    string = string.split(',')
+    for word in string:
+        print(word)
+
 
 if __name__ == "__main__":
     main()
