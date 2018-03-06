cfa259ecb56fa96a1385e8a3964ead617f1b5a45
grudisil <grudisil@eecs.wsu.edu>
2018-03-06 00:14:54 -0800

    last commit for the night

--- code355/py/HW3.py
+++ code355/py/HW3.py
@@ -110,7 +110,17 @@ def lookupVal2(lst,val):
 #problem 4
 
 #problem 5
-
+def numberOfPaths(m, n):
+   # If either given row number is first
+   # or given column number is first
+   if(m == 1 or n == 1):
+        return 1
+   
+   # If diagonal movements are allowed
+   # then the last addition
+   # is required.
+   return  numberOfPaths(m-1, n) + numberOfPaths(m, n-1)
+ 
 #problem 6
 
 #problem 7
@@ -132,6 +142,8 @@ def main():
 
     val = lookupVal([{"x":1,"y":True,"z":"found"},{"x":2},{"y":False}],"y")
     print(val)
+
+    print(numberOfPaths(3,3))
 
 
 if __name__ == "__main__":
