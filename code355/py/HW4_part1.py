099ddcee9ea7007e5c9a0efe3dbdc9e6cce522b8
dak0ta <dak0ta@localhost.localdomain>
2018-03-18 19:46:36 -0700

    code skeleton in place

---
+++ code355/py/HW4_part1.py
@@ -0,0 +1,64 @@
+#Garrett Rudisill
+#CptS 355 HW4 part 1
+
+#opstack portion
+opstack = []
+
+def opPop():
+    pass
+
+def opPush(val):
+    pass
+
+#dictionary stack
+#-----------------------
+dictstack = []
+
+def dictPop():
+    pass
+
+def dictPush(d):
+    pass
+
+def define(name,val):
+    pass
+
+def lookup(name):
+    pass
+
+#we gon shoot at ops
+#-----
+#add
+#sub
+#mul
+#div
+#mod
+
+#array ops
+#---------
+#length
+#get
+
+#stack manip ops
+#--------
+#dup, exch, pop, roll , copy, clear, stack
+
+#dick manip ops
+#------
+#psDict, begin, end, psDef
+
+
+#testfunction
+def tests():
+    print("Hope your code isnt ass\n")
+    pass
+
+def main():
+    mytests = True
+    if mytests == True:
+        print("Running my tests, set mytests at line 336 to false to disable\n")
+        tests()
+    else:
+        print("Executing your tests")
+if __name__ == "__main__":
+    main()
