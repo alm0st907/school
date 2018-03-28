4f80cda7b9dad43a0a3059b2369b3f9c83a5c6d8
grudisil <grudisil@eecs.wsu.edu>
2018-03-28 16:17:15 -0700

    Added part2 file for HW4_p2 start

---
+++ code355/py/HW4_part2.py
@@ -0,0 +1,283 @@
+#Garrett Rudisill
+#CptS 355 HW4 part 1
+#WSU ID 11461816
+#PostScript interpreter part 1 in python 3.6
+#tested on both linux and windows
+
+#opstack portion
+opstack = []
+
+def opPop():
+    #pop a item off stack if its not empty
+    if len(opstack) > 0:
+        return opstack.pop()
+    else:
+        print("Op stack is empty")
+
+def opPush(val):
+    #push things to the stack
+    #functions error check, so we dont need to check whats pushed
+    opstack.append(val)
+
+#dictionary stack
+#-----------------------
+dictstack = []
+
+def dictPop():
+    #pop from dictionary if there is an item inside
+    if len(dictstack) > 0:
+        return dictstack.pop()
+    else:
+        print("Dict stack is empty")
+
+def dictPush(d):
+    #append to dictstack
+    dictstack.append(d)
+
+def define(name,val):
+    if name[0] =='/':#check for / character symbolizing a var def
+        name = name[1:]#rem /
+        if dictstack == []:#if dictionary stack empty
+            d = {}
+            d[name] = val
+            dictstack.append(d) #append new dict with val to dictstack
+        else:
+            (dictstack[-1])[name]=val #update if not empty
+    else:
+        print("variable not defined")
+
+#check this
+def lookup(name):
+    operable_dict = reversed(dictstack) #if more than one dict, ensure from top of stack to bottom
+    for d in operable_dict:
+        if name in d:
+            return d[name]
+    else:
+        print("Name is not defined")
+
+#we gon shoot at ops
+#-----
+#add
+#sub 
+#mul
+#div
+#mod
+def add():
+    try:
+        op1 = opPop()
+        op2 = opPop()
+        #type checking
+        if isinstance(op1,(int,float)) and isinstance(op2,(int,float)):
+            opstack.append(op1+op2)
+        else:
+            print("Invalid inputs")
+    except:
+        print("not enough args")
+
+def sub():
+    try:
+        op1 = opPop()
+        op2 = opPop()
+        #type checking
+        if isinstance(op1,(int,float)) and isinstance(op2,(int,float)):
+            opstack.append(op2-op1)
+        else:
+            print("Invalid inputs")
+    except:
+        print("not enough args")
+
+def mul():
+    try:
+        op1 = opPop()
+        op2 = opPop()
+        #type checking
+        if isinstance(op1,(int,float)) and isinstance(op2,(int,float)):
+            opstack.append(op2*op1)
+        else:
+            print("Invalid inputs")
+    except:
+        print("not enough args")
+
+def div():
+    try:
+        op1 = opPop()
+        op2 = opPop()
+        if op2 == 0:
+            print("div by 0 error")
+            return
+        elif op1 == 0 and op2 == 0:
+            print("div by 0 error")
+            return
+        elif op1 == 0:
+            opstack.append(0)
+            return
+        else:
+            pass
+        #type checking
+        if isinstance(op1,(int,float)) and isinstance(op2,(int,float)):
+                opstack.append(op2/op1)
+        else:
+            print("invalid inputs")
+    except:
+        print("not enough args")
+
+def mod():
+    try:
+        op1 = opPop()
+        op2 = opPop()
+        #type checking
+        #mods are probably int only?
+        if isinstance(op1,int) and isinstance(op2,int):
+            opstack.append(op2%op1)
+        else:
+            print("invalid ops")
+    except:
+        print("not enough args")
+
+
+#array ops
+#---------
+#length
+#get
+
+def length():
+    temp = opPop()
+    if isinstance(temp, list): #check for array before len call
+        opPush(len(temp))
+    else:
+        print("type error")
+
+def get():
+    ar_ind = opPop()
+    arr = opPop()
+    if isinstance(arr, list):
+        try:
+            opPush(arr[ar_ind])
+        except:
+            print("out of index error")
+    else:
+        print("type error - not array")
+
+    
+#stack manip ops
+#--------
+#dup, exch, pop, roll , copy, clear, stack
+
+def dup():
+    op1 = opPop()
+    opstack.append(op1)
+    opstack.append(op1)
+    #works, could be more efficient, but I dont really care
+
+def exch():
+    op1 = opPop()
+    op2 = opPop()
+    if op1 == None or op2 == None:
+        print("cant exchange some with none")
+        return
+    opstack.append(op1)
+    opstack.append(op2)
+    #ol' reddit switcheroo
+
+def pop():
+    _ = opPop()
+    #trash a op
+
+def clear():
+    opstack.clear()
+    #nuclear launch @ stack detected
+
+def copy():
+    try:#how many items to copy
+        numItems = int(opPop()) #cast to an int if possible
+    except:
+        print("Item is null/unsucessfully converted to int")
+        return
+    if numItems > len(opstack) or numItems < 0:
+        print("negative items or attempting to copy nonexistent items")
+        return
+    else:
+        #operation execution
+        i = 0
+        temp = []
+        operableStack = list(reversed(opstack))
+        #reversed for easy linear traversal
+        while i < numItems: #iterate for i number of items
+            #get num items
+            temp.append(operableStack[i])
+            i+=1            
+        for val in temp:#appened all the values that need to be copied
+            opstack.append(val)
+        
+
+    
+
+def roll():
+    if len(opstack) <=2 : #stack 2 short
+        print("not enough args, cant roll")
+        return
+    rolls = opPop() # num of rolls
+    items = opPop() # num of items to be rolled
+    if not isinstance(rolls,int) or not isinstance(items,int): #datatype check
+        print("invalid args")
+        return
+    elif items < 0:
+        print("invalid index")
+        return
+    elif items <2:
+        return #no change to stack
+    
+    else: #actually perform op
+        if rolls >=0 : #roll data to bottom from top
+            items = -items +1 #index correction
+            for count in range(rolls):
+                val = opPop()
+                opstack[items:items-1]=[val]
+        else:
+            #roll data to top from bottom
+            items = -items
+            rolls = -rolls
+            for count in range(rolls):
+                val = opPop()
+                opstack[items:items-1]=[val]
+
+#print le stack
+def stack():
+    printable = reversed(opstack)
+    for ent in printable:
+        print(ent)
+
+#dick manip ops
+#------
+#psDict, begin, end, psDef
+def psDict():
+    if opPop() != None:
+        opPush({})
+    else:
+        print("operation not completed")
+
+#push a dict to dictstack
+def begin():
+    temp = opPop()
+    if isinstance(temp, dict):
+        dictPush(temp)
+    else:
+        print("not a dictionary")
+
+#pop off dictstact
+def end():
+    if dictstack != None:
+        dictPop()
+    else:
+        print("dict empty, nothing to pop from end")
+
+#define stuff
+def psDef():
+    val = opPop()
+    name = opPop()
+    define(name,val)
+
+
+
+if __name__ == '__main__':
+    print("not finished lols")
