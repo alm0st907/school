1acd74bd5d002e78ba848e8604ce19be0ece8d5c
dak0ta <dak0ta@localhost.localdomain>
2018-04-09 11:46:37 -0700

    adding some hello world code

---
+++ code355/coffee/hi.java
@@ -0,0 +1,9 @@
+public class hi //class name and file need to be same
+{
+  public static void main(String[] args)//tpical main
+  {
+    System.out.print("it still works\n");//basically c# again
+    System.out.println("Hi there bois");
+
+  }
+}
