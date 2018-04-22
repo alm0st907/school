73cc08db53082632d11ce7868c6ab67c4b967614
dak0ta <dak0ta@localhost.localdomain>
2018-04-22 16:21:56 -0700

    hit stats start

--- code355/coffee/BallGame/Player.java
+++ code355/coffee/BallGame/Player.java
@@ -1,12 +1,20 @@
-
+import java.util.*;
 public class Player
 {
     public int score;
     public int hits;
+    public Dictionary stats;
+
     public Player()
     {
         score = 0;
         hits = 0;
+        //dictionary of hits
+        stats.put("basic", 0);
+        stats.put("shrink", 0);
+        stats.put("bounce", 0);
+        stats.put("split", 0);
+                
     }
     public void UpdateScore(int points)
     {
